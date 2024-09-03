8f8c80585716aeb16ba6b16a666603ea792f3835
ST-SURYA <surya.b@arkinfotec.com>
2024-09-03 15:40:03 +0530

    bug fix

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -3,38 +3,42 @@ import AuthenticationForm from "../components/form/AuthenticationForm";
 import { Link, useNavigate } from "react-router-dom";
 import client from "../libs/client";
 import { Card } from "@/components/ui/card";
+import Layout from "@/components/Layout";
 import { useToast } from "@/hooks/use-toast";
 
-const Registration = () => {
+const Login = () => {
     const navigate = useNavigate();
     const { toast } = useToast();
 
     const onSubmit = (values: FormValues) => {
-        client.signUpUser.mutation({ body: values }).then(() => {
-            navigate("/login");
+        client.loginUser.mutation({ body: { ...values } }).then((res) => {
+            localStorage.setItem("token", res.data.token);
+            navigate("/");
         }).catch((res) => {
-            console.log(res?.response.data.message);  // Check error message in console
             toast({
                 variant: "destructive",
                 description: `${res?.response.data.message}`,
-            });
+            })
         });
     };
-
     return (
-        <Card className="flex items-center justify-center min-h-screen">
-            <div className="w-72 p-6 shadow-md rounded-md">
-                <h1 className="text-2xl text-center font-semibold mb-4">Sign Up</h1>
-                <AuthenticationForm onSubmit={onSubmit} />
-                <p className="text-center mt-4 text-sm">
-                    Already have an account?{" "}
-                    <Link to="/login" className="text-blue-500">
+        <Layout>
+            <Card className="flex items-center justify-center min-h-screen">
+                <div className="w-72 p-6 shadow-md rounded-md">
+                    <h1 className="text-2xl text-center font-semibold mb-4">
                         Login
-                    </Link>
-                </p>
-            </div>
-        </Card>
-    );
+                    </h1>
+                    <AuthenticationForm onSubmit={onSubmit} />
+                    <p className="text-center mt-4 text-sm">
+                        Don't have an account?{" "}
+                        <Link to="/signup" className="text-blue-500">
+                            Sign Up
+                        </Link>
+                    </p>
+                </div>
+            </Card>
+        </Layout>
+    )
 }
 
-export default Registration;
+export default Login;
