4aa9e911d43d9ded2deecaa9de83ccb255c71898
ST-SURYA <surya.b@arkinfotec.com>
2024-09-25 19:35:59 +0530

    UI work -pending

--- client/src/router/Router.tsx
+++ client/src/router/Router.tsx
@@ -3,11 +3,14 @@ import Login from "../pages/Login";
 import Registration from "../pages/Registration";
 import Home from "../pages/Home";
 import Accounts from "@/components/Accounts";
+// import Accounts from "@/components/Accounts";
+import LoginNew from "@/pages/LoginNew";
+import Main from "@/pages/Main";
 
 const router = createBrowserRouter([
     {
         path: "/login",
-        element: <Login />,
+        element: <LoginNew />,
         index: true
     },
     {
