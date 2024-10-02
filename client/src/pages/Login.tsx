d6666ab72da6445a9d326caf6c05bd3887b5165c
ST-SURYA <surya.b@arkinfotec.com>
2024-10-02 10:25:57 +0530

    optimization

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,44 +1,98 @@
-import type { FormValues } from "./types";
-import AuthenticationForm from "../components/form/AuthenticationForm";
-import { Link, useNavigate } from "react-router-dom";
-import client from "../libs/client";
-import { Card } from "@/components/ui/card";
-import Layout from "@/components/Layout";
-import { useToast } from "@/hooks/use-toast";
+import { Link } from "react-router-dom";
+import { Field, Form } from "react-final-form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { FcGoogle } from "react-icons/fc";
+import { ModeToggle } from "@/components/ThemeToggler";
+
 
 const Login = () => {
-    const navigate = useNavigate();
-    const { toast } = useToast();
 
-    const onSubmit = (values: FormValues) => {
-        client.loginUser.mutation({ body: { ...values } }).then((res) => {
-            localStorage.setItem("token", res.data.token);
-            navigate("/");
-        }).catch((res) => {
-            toast({
-                variant: "destructive",
-                description: `${res?.response.data.message}`,
-            })
-        });
-    };
     return (
-        <Layout>
-            <Card className="flex items-center justify-center min-h-screen">
-                <div className="w-72 p-6 shadow-md rounded-md">
-                    <h1 className="text-2xl text-center font-semibold mb-4">
-                        Login
-                    </h1>
-                    <AuthenticationForm onSubmit={onSubmit} />
-                    <p className="text-center mt-4 text-sm">
+        <div className="relative flex">
+            <div className="absolute top-4 right-4">
+                <ModeToggle />
+            </div>
+                <div className="left-container hidden lg:block lg:w-1/2">
+                <img src="../../public/pixlr-image-generator-035992d1-c4f4-466f-b436-ac8046e8fca4.png"/>
+                </div>
+                <div className="right-container w-full lg:w-1/2 flex items-center justify-center min-h-screen">
+                    <div className="text-center">
+                        <h1 className="text-2xl font-semibold mb-4">
+                        Welcome Back
+                        </h1>
+                        <span className="block whitespace-nowrap mb-6">
+                        Please log in to your account
+                        </span>
+                        <Form
+                            onSubmit={() => { }}
+                            initialValues={{ email: "", password: "" }}
+                            render={({ handleSubmit, form, submitting, }) => (
+                                <form onSubmit={handleSubmit}>
+                                    <div className="mb-4">
+                                        <Field
+                                            name="email"
+                                            render={({ input, meta }) => (
+                                                <>
+                                                    <Input
+                                                        {...input}
+                                                        type="email"
+                                                        placeholder="name@example.com"
+                                                    />
+                                                    {meta.error && meta.touched && (
+                                                        <span className="text-red-500">{meta.error}</span>
+                                                    )}
+                                                </>
+                                            )}
+                                        />
+                                    </div>
+                                    <div className="mb-6">
+                                        <Field
+                                            name="password"
+                                            render={({ input, meta }) => (
+                                                <>
+                                                    <Input
+                                                        {...input}
+                                                        type="password"
+                                                        placeholder="Enter your Password"
+                                                    />
+                                                    {meta.error && meta.touched && (
+                                                        <span className="text-red-500">{meta.error}</span>
+                                                    )}
+                                                </>
+                                            )}
+                                        />
+                                    </div>
+                                    <div className="flex justify-center mt-4">
+                                        <Button
+                                            type="submit"
+                                            className="login-field"
+                                            disabled={submitting}
+                                        >
+                                            LogIn with Email
+                                        </Button>
+                                    </div>
+                                </form>
+                            )}
+                        />
+                        <div className="flex items-center my-6">
+                            <div className="flex-grow border-t border-gray-300"></div>
+                            <span className="mx-4 text-gray-500">Or continue with</span>
+                            <div className="flex-grow border-t border-gray-300"></div>
+                        </div>
+                        <div className="flex justify-center mt-4">
+                            <FcGoogle size={20}/>
+                        </div>
+                        <p className="text-center mt-4 text-sm">
                         Don't have an account?{" "}
                         <Link to="/signup" className="text-blue-500">
                             Sign Up
-                        </Link>
-                    </p>
+                            </Link>
+                        </p>
+                    </div>
                 </div>
-            </Card>
-        </Layout>
-    )
+            </div>
+    );
 }
 
 export default Login;
--- client/src/router/Router.tsx
+++ client/src/router/Router.tsx
@@ -1,16 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
-import Login from "../pages/Login";
 import Registration from "../pages/Registration";
 import Home from "../pages/Home";
 import Accounts from "@/components/Accounts";
 // import Accounts from "@/components/Accounts";
 import LoginNew from "@/pages/LoginNew";
 import Main from "@/pages/Main";
+import Login from "@/pages/Login";
 
 const router = createBrowserRouter([
     {
         path: "/login",
-        element: <LoginNew />,
+        element: <Login />,
         index: true
     },
     {
